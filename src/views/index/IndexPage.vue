15b90f89bbfce54209dedbc1e5f6a9f2181f3dad
maslov28 <artem.fatefulchoice@mail.ru>
2022-07-30 00:20:28 +0300

    added comparison section

--- src/views/index/IndexPage.vue
+++ src/views/index/IndexPage.vue
@@ -4,6 +4,7 @@
     <benefits-section></benefits-section>
     <adventages-section></adventages-section>
     <competencies-section></competencies-section>
+    <comparison-section></comparison-section>
   </div>
 </template>
 
@@ -12,13 +13,15 @@ import StartSection from '@/components/blocks/start-section/StartSection.vue'
 import BenefitsSection from '@/components/blocks/benefits-section/BenefitsSection.vue'
 import AdventagesSection from '@/components/blocks/adventages-section/AdventagesSection.vue'
 import CompetenciesSection from '@/components/blocks/competencies-section/CompetenciesSection.vue'
+import ComparisonSection from '../../components/blocks/comparison-section/ComparisonSection.vue'
 
 export default {
   components: {
     StartSection,
     BenefitsSection,
     AdventagesSection,
-    CompetenciesSection
+    CompetenciesSection,
+    ComparisonSection
 }
 }
 </script>
